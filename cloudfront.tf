3bc4de1aff54b0dc25a35f470339caa68128c397
Faizan Younus <50990523+ifaizan@users.noreply.github.com>
2021-07-08 19:27:40 +0500

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -4,23 +4,23 @@ resource "aws_cloudfront_origin_access_identity" "myOAI" {
 
 resource "aws_cloudfront_distribution" "my-static-content-cdn" {
   origin {
-   domain_name = aws_s3_bucket.bucket-for-static-web.bucket_regional_domain_name
-   origin_id = "primaryS3" 
+    domain_name = aws_s3_bucket.bucket-for-static-web.bucket_regional_domain_name
+    origin_id   = "primaryS3"
 
-   s3_origin_config {
+    s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.myOAI.cloudfront_access_identity_path
     }
   }
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = "primaryS3"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "primaryS3"
     viewer_protocol_policy = "allow-all"
-	  
     forwarded_values {
       query_string = false
       headers      = ["Origin"]
+
       cookies {
         forward = "none"
       }
@@ -30,9 +30,9 @@ resource "aws_cloudfront_distribution" "my-static-content-cdn" {
   enabled = true
 
   restrictions {
-    geo_restriction {	
+    geo_restriction {
       restriction_type = "none"
-	}
+    }
   }
 
   viewer_certificate {
