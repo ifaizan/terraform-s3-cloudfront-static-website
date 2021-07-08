1733b736dfb39a6cf38aaeb8f22a9ac4056db9b3
Faizan Younus <50990523+ifaizan@users.noreply.github.com>
2021-07-08 19:24:30 +0500

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -13,14 +13,14 @@ resource "aws_cloudfront_distribution" "my-static-content-cdn" {
   }
 
   default_cache_behavior {
-	allowed_methods = ["GET", "HEAD"]
+    allowed_methods = ["GET", "HEAD"]
     cached_methods = ["GET", "HEAD"]
     target_origin_id = "primaryS3"
-	viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "allow-all"
+	  
     forwarded_values {
       query_string = false
       headers      = ["Origin"]
-
       cookies {
         forward = "none"
       }
